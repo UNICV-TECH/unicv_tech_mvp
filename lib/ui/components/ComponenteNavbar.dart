c4b79b68dda6d8a71a424e81e6c52377f08c0819
Gabriella Freitas <gsf.gabriellasantos@gmail.com>
2025-10-01 14:22:47 -0300

    feat: ajustando erro

--- lib/ui/components/ComponenteNavbar.dart
+++ lib/ui/components/ComponenteNavbar.dart
@@ -63,7 +63,7 @@ class _CustomNavBarState extends State<CustomNavBar> {
                   boxShadow: [
                     BoxShadow(
                       // ignore: deprecated_member_use
-                      color: Colors.black.withOpacity(0.2),
+                      color: Colors.black.withValues(alpha: 0.20),
                       spreadRadius: 2,
                       blurRadius: 10,
                       offset: const Offset(0, 5),
@@ -86,7 +86,7 @@ class _CustomNavBarState extends State<CustomNavBar> {
                 shape: BoxShape.circle,
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.20),
+                    color: Colors.black.withValues(alpha: 0.2),
                     blurRadius: 10,
                     spreadRadius: 2,
                     offset: const Offset(0, 5),
